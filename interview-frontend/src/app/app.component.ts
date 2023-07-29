1e855fe9bd71d7190d2701d6b7652310b24afad0
JacoLombardo <jacopo.lombardo@outlook.com>
2023-07-29 14:31:07 +0200

    http

--- interview-frontend/src/app/app.component.ts
+++ interview-frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { CityFormComponent } from './city-form/city-form.component';
+import { ApiserviceService } from './Service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'interview-frontend';
+
+  newdata: any;
+  constructor(private apiservie: ApiserviceService) {}
+
+  ngOnInit() {
+    console.log(
+      this.apiservie.getData().subscribe((res) => {
+        this.newdata = res;
+      })
+    );
+    console.log(this.newdata);
+  }
 }
--- interview-frontend/src/app/city-form/city-form.component.ts
+++ interview-frontend/src/app/city-form/city-form.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Search } from 'src/search';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-city-form',
@@ -16,17 +17,25 @@ export class CityFormComponent {
 
   constructor(private http: HttpClient) {}
 
-  public getFilteredCities(search: string): any {
-    const url = 'https://localhost:3000?search=be';
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append('search', search);
+  public getFilteredCities(search: string): Observable<any> {
+    const url = 'https://age-of-empires-2-api.vercel.app/api/civilizations/all';
+    // const url = 'https://localhost:3000?search=be';
+    // let queryParams = new HttpParams();
+    // queryParams = queryParams.append('search', search);
 
-    return this.http.get<any>(url, { params: queryParams });
+    // return this.http.get(url, { params: queryParams }).pipe(
+    //   map((data) => {
+    //     console.log('1', data);
+    //   })
+    // );
+
+    return this.http.get(url);
   }
 
   onSubmit() {
     this.submitted = true;
-    this.getFilteredCities(this.model.name);
+    const test = this.getFilteredCities(this.model.name);
     console.log(this.getFilteredCities(this.model.name));
+    console.log(test);
   }
 }
